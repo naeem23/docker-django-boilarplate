7184157043d877e7c66426ee3edd6faf3d2b4ceb
s.naeem <siddique.naeem@dynamiconsulting.co.uk>
2023-07-06 11:43:42 +0600

    updated settings for static file

--- leafletdistribution/settings/base.py
+++ leafletdistribution/settings/base.py
@@ -82,7 +82,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
